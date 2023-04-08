f48d7a8e709a1a53ca5e74112b47fbb837dd48b1
pathfinder1549 <mhilliard117@gmail.com>
2023-04-08 13:36:13 -0400

    2 button test, moved libs around

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,38 @@
 #include <Arduino.h>
+#include <Joystick.h>
 
-int buttonState;
+const int BUTTON_COUNT = 2;
+const int HAT_COUNT = 0;
+int lastButtonState[BUTTON_COUNT];
+
+Joystick_ buttonBox(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_GAMEPAD,
+  BUTTON_COUNT, HAT_COUNT,
+  false, false, false, false, false, false, false, false, false, false, false);
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(0, OUTPUT);
-  pinMode(5, INPUT_PULLUP);
+  pinMode(1, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
+
+  buttonBox.begin();
+
+  for (int i=0; i<BUTTON_COUNT; i++) {
+    lastButtonState[i] = 0;
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  buttonState = digitalRead(5);
-  if (buttonState == 1) {
+  for (int index=0; index<BUTTON_COUNT; index++) {
+    int currentButtonState = !digitalRead(index + 1);
+    if (currentButtonState != lastButtonState[index]) {
+      buttonBox.setButton(index, currentButtonState);
+      lastButtonState[index] = currentButtonState;
+    }
+  }
+
+  if (lastButtonState[1] == 1) {
     digitalWrite(0, 1);
   } else {
     digitalWrite(0, 0);
